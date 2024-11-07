dd6fc86a43a109ac9a5b8b8ccb16660e8c98a737
RageRaghav <9560397344r@gmail.com>
2024-11-07 23:16:06 +0530

    First commit

---
+++ Day 5/script.js
@@ -0,0 +1,13 @@
+// let disp=()=>{
+//     console.log("Welcome to arrow function in js");
+
+// };
+// disp();
+// let say=()=>console.log("arrow function");
+// say();
+
+// let sum=(x,y)=>{
+//     return x+y;
+// };
+// let res=sum(5,6);
+// console.log(res);
